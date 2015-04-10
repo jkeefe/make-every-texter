a14d2bba38b5cfdfd45a4549279073868d92143f
John Keefe <john@johnkeefe.net>
2015-04-09 22:41:46 -0400

    starting out with a simple parser

---
+++ parser.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+// The next line reads in a file with my api keys.
+// Delete it if you hard-code your keys into the code below
+var keys = require('../api_keys/readability_keys'); 
+
+var textedUrl = "http://johnkeefe.net/make-every-week-remote-controlled-egg";
+    // Values read in:
+    // READABILITY_PARSER_TOKEN      (called keys.READABILITY_PARSER_TOKEN)
+
+
+parseIt(textedUrl);
+
+
+// This function sends a URL to the Readability API 
+// and outputs the results to the console
+function parseIt(parseUrl) {
+    
+    // configure the request
+    var options = {
+        url: "https://readability.com/api/content/v1/parser/?url=" + parseUrl + 
+            "&token=" + keys.READABILITY_PARSER_TOKEN,
+        method: "GET"
+    };
+    
+    // make the request
+    request(options, function (error, response, body){
+        if (!error && response.statusCode == 200) {
+            
+            // Parse the JSON from the body
+            var content = JSON.parse(body);
+            
+            // success! print body we got back
+            console.log("Title:", content.title);
+            console.log("Author:", content.author);
+            console.log("Excerpt:", content.excerpt);
+            console.log("Image:", content.lead_image_url);
+            console.log("Date:", content.date_published);
+            
+        } else {
+            
+            // otherwise output an error
+            console.log(error);
+            
+        }
+        
+    });
+    
+    
+}
